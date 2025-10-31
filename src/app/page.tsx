475af0ccaf2c3b8c2b7ac67339f81de59d3b6d85
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-10-31 19:15:52 -0300

    feat: add ui for sign in and sign up form components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { Button } from "@/components/ui/button";
-
 function Home() {
   return (
     <div className="bg-amber-300">
       <h1>Hello World</h1>
-      <Button>
-        Clcik me
-      </Button>
     </div>
   );
 }
 
 export default Home;
+
