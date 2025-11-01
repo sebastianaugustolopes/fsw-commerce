72293b362d77ae868d210d472b61fe2271af65ca
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-11-01 18:21:22 -0300

    feat: add Google sign-in option and update sign-in form layout

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,21 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
+import { db } from "@/db/index";
 import {
   accountTable,
   sessionTable,
   userTable,
   verificationTable,
 } from "@/db/schema";
-import { db } from "@/db/index";
 
 export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
