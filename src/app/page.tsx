9759c06c54e8eecc5f9ae759df747f5d7cb5adad
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-10-31 22:09:24 -0300

    feat: add sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from "../components/common/header";
+
 function Home() {
   return (
-    <div className="bg-amber-300">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
