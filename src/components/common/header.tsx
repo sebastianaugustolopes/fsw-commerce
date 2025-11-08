6658e3f913bf6839d471a961faf84377d2eb4294
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-11-08 15:54:58 -0300

    feat: ensure product can be removed from cart

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -14,6 +14,7 @@ import {
   SheetTrigger,
 } from "@/src/components/ui/sheet";
 import { authClient } from "@/lib/auth-client";
+import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
@@ -25,7 +26,8 @@ export const Header = () => {
         </div>
       </Link>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
+        <Cart />
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
