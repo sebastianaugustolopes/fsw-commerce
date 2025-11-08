cca608d4c1761a584d4db8ec1c440a77b5df3209
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-11-08 17:26:47 -0300

    feat: add address form

---
+++ src/app/cart/identification/page.tsx
@@ -0,0 +1,38 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+import { Header } from "@/src/components/common/header";
+import { db } from "@/src/db";
+import { cartTable } from "@/src/db/schema";
+
+import Addresses from "./components/addresses";
+
+const IdentificationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user.id) {
+    redirect("/");
+  }
+  const cart = await db.query.cartTable.findFirst({
+    where: eq(cartTable.userId, session.user.id),
+    with: {
+      items: true,
+    },
+  });
+  if (!cart || cart?.items.length === 0) {
+    redirect("/");
+  }
+  return (
+    <>
+      <Header />
+      <div className="px-5">
+        <Addresses />
+      </div>
+    </>
+  );
+};
+
+export default IdentificationPage;
